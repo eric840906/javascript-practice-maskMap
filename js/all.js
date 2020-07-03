d9948a2f84f872f3fa26ce57ae1e581531f51dcb
eric840906 <ecericchiu1@gmail.com>
2020-07-03 22:18:11 +0800

    RWD UI optimize

--- js/all.js
+++ js/all.js
@@ -6212,14 +6212,11 @@ showDay();
 (function($) {
 
 	/*"use strict";
-
 	var fullHeight = function() {
-
 		$('.js-fullheight').css('height', $(window).height());
 		$(window).resize(function(){
 			$('.js-fullheight').css('height', $(window).height());
 		});
-
 	};
 	fullHeight();*/
 
